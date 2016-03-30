84a281ea1d8c9d8e964a5c9ccb68b0b2ea2ac4ea
Danny Morey <dannymorey@gmail.com>
2016-03-30 14:27:18 +0100

    First commit

---
+++ 403.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>
+            404: Page Not Found
+    </title>
+    <style>
+    * { font-family: verdana; font-size: 10pt; color: gray; }
+    b { font-weight: bold; }
+    table { height: 50%; border: 1px solid gray;}
+    td { text-align: center; padding: 25px;}
+    </style>
+</head>
+<body>
+<center>
+<br><br><br><br>
+	<table>
+	<tr><td><div style="font-size:50px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">403<br/><span style="font-size:20px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">Access denied</span></div></td></tr>
+	<tr><td>You do not have permission to view this page</td></tr>
+        <tr><td style="font-size: 8pt"><a href="javascript:history.back()">Back</a></td></tr>
+	</table>
+<br><br>
+
+</center>
+</body>
+
+</html>
---
+++ 404.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>
+            404: Page Not Found
+    </title>
+    <style>
+    * { font-family: verdana; font-size: 10pt; color: gray; }
+    b { font-weight: bold; }
+    table { height: 50%; border: 1px solid gray;}
+    td { text-align: center; padding: 25px;}
+    </style>
+</head>
+<body>
+<center>
+<br><br><br><br>
+	<table>
+	<tr><td><div style="font-size:50px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">404<br/><span style="font-size:20px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">oops! page not found</span></div></td></tr>
+	<tr><td>The page you were looking for could not be found.</td></tr>
+        <tr><td style="font-size: 8pt"><a href="/index.php">Homepage</a></td></tr>
+	</table>
+<br><br>
+
+</center>
+</body>
+
+</html>
