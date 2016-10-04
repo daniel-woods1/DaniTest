34eb39da159f7feae8ef9805719edce1ea436e4f
Danny Morey <dannymorey@gmail.com>
2016-10-04 11:32:59 +0100

    Danny file added 04102016

---
+++ danny.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>
+            This is a test
+    </title>
+    <style>
+    * { font-family: verdana; font-size: 10pt; color: gray; }
+    b { font-weight: bold; }
+    table { height: 50%; border: 1px solid gray;}
+    td { text-align: center; padding: 25px;}
+    </style>
+</head>
+<body>
+<center>
+<br><br><br><br>
+	<table>
+	<tr><td><div style="font-size:50px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">404<br/><span style="font-size:20px; color:red;text-shadow: 1px 1px #000;font-weight:bold;">oops! page not found</span></div></td></tr>
+	<tr><td>Test File</td></tr>
+        <tr><td style="font-size: 8pt"><a href="/index.php">Test</a></td></tr>
+	</table>
+<br><br>
+
+</center>
+</body>
+
+</html>
